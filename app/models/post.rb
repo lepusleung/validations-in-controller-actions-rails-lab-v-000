b00997477d28820953228f37da4f1aaec2e276f7
Man Leung <lepusleung@gmail.com>
2018-06-18 15:06:44 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 100 }
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"],   message: "%{value} is not a valid category. Must be either 'Fiction' or 'Non-Fiction'."}
+
 end
